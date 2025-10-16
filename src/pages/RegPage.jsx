55a34bcdaad879ddcde797581d4dcc14cad1c8b0
ivnls <il4820728@gmail.com>
2025-10-16 06:52:48 -0300

    Enable Captcha

--- src/pages/RegPage.jsx
+++ src/pages/RegPage.jsx
@@ -6,6 +6,7 @@ import { useScore } from "../context/ScoreContext";
 import { useDate } from "../context/DateContext";
 import ReCAPTCHA from 'react-google-recaptcha';
 import cidades from "../components/db/cidades.json"
+import RelatorioResultados from "../components/RelatorioPCidade";
 
 import { supabase } from "../utils/supaBaseClient";
 
@@ -92,7 +93,7 @@ function RegForm() {
             e++;
         }
 
-        /*if (!isCaptchaVerified) {
+        if (!isCaptchaVerified) {
             setErroCaptcha("Por favor, conclua o Captcha");
             e++;
         } else {
@@ -100,7 +101,7 @@ function RegForm() {
             setIsCaptchaVerified(false);
             setErroCaptcha("");
         }
-*/
+
         if (e > 0) {
             return;
         }
@@ -184,13 +185,13 @@ function RegForm() {
             </select>
             <p className="text-red-700 bg-red-200 rounded-md my-2 mx-4">{erroCidade}</p>
 
-            {/*<div className="w-full flex justify-center mt-4">
+            <div className="w-full flex justify-center mt-4">
                 <ReCAPTCHA
                 ref={captchaRef}
                 sitekey={import.meta.env.VITE_RECAP_KEY}
                 onChange={handleCaptchaChange}
                 />
-            </div>*/}
+            </div>
             <p className="text-red-700 bg-red-200 rounded-md my-2 mx-4">{erroCaptcha}</p>
 
             <button type="submit" className="mt-6 px-10 bg-blue-700 text-white p-2 rounded-md hover:bg-blue-800 transition">Registrar</button>
