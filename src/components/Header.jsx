36c1cb205c4b18169c0598e816b6eb4636557241
Ivnls <il4820728@gmail.com>
2025-11-08 16:39:07 -0300

    fix header on cll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ function Header() {
     return (
         <header className="flex flex-col lg:flex-row w-full items-center justify-between gap-10 border-b-8 rounded-b-3xl bg-gradient-to-t from-blue-950 to-cyan-600 border-slate-800 text-white">
             
-            <div className="flex flex-col items-center lg:py-3 pl-6 pt-4">
-                <img src={logo} className="h-32 lg:m-2 w-auto" alt="Logo M-CHAT" />
+            <div className="flex flex-col items-center lg:py-3 lg:pl-6 pt-4">
+                <img src={logo} className="h-24 lg:h-32 lg:m-2 w-auto" alt="Logo M-CHAT" />
             </div>
 
             <div className="flex flex-row items-center gap-5 px-5 pb-4">
